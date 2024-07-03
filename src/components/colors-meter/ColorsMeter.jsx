be99b5ccaafb7e030509be2461ee51bb3cece9de
ShailaGonzalez <shailagonzalezcarrero@gmail.com>
2024-07-03 21:17:47 +0200

    upload component

---
+++ src/components/colors-meter/ColorsMeter.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {SectionTotalContainer, SectionMeter, ImageMeter, SectionInfo, ContainerGood, ContainerModerate, ContainerSensitive,ContainerUnhealthy, ContainerVeryUnhealthy, ContainerHazardous} from "./ColorsMeter.scss"
+
+import ImageMeter from "../../assets/images";
+
+const ColorsMeter = () => {
+    return (
+        <>
+<SectionTotalContainer>
+
+<SectionMeter>
+<ImageMeter src = {ImageMeter} alt = "ImageMeter" />
+</SectionMeter>
+
+<SectionInfo>
+
+<ContainerGood>
+<Description>
+    Air quality is considereded satisfactory.
+</Description>
+</ContainerGood>
+
+<ContainerModerate>
+    <Description>
+Air quality is acceptable; however, a very small number of people who are unusually sensitive to air pollution.
+    </Description>
+</ContainerModerate>
+
+
+<ContainerSensitive>
+    <Description>
+Members of sensitive groups may experience health effects.
+    </Description>
+</ContainerSensitive>
+
+
+<ContainerUnhealthy>
+    <Description>
+        Everyone may experience more serious health effects
+    </Description>
+</ContainerUnhealthy>
+
+
+<ContainerVeryUnhealthy>
+    <Description>
+        Everyone may experience more serious health effects.
+    </Description>
+</ContainerVeryUnhealthy>
+
+<ContainerHazardous>
+    <Description>
+Health warnings of emergency conditions. The entire population is more likely to be affected.
+    </Description>
+</ContainerHazardous>
+
+
+</SectionInfo>
+
+</SectionTotalContainer>
+
+</>
+    );
+}
+
+export default ColorsMeter;
