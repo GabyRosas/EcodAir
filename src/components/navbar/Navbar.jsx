0b43ea442ea918598b76bab85eeca58fe75a5351
sandiaxcx <sandra8x@gmail.com>
2024-07-04 15:06:13 +0200

    fix problems

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,11 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 // import { NavLink } from "react-router-dom";
 import './Navbar.css';
-
 const Navbar = () => {
     return (
-
-
+        <navbar className="navbar">
+            <h1 id="EcoAirh1">EcodAir</h1>
+        </navbar>
     );
 }
-
 export default Navbar;
