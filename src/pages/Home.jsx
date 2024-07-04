ce60321974809d42bcb43778fd9e221ae548f7a5
sandiaxcx <sandra8x@gmail.com>
2024-07-04 09:57:12 +0200

    create landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import Home from "../components/home/Home";
 
 const Home = () => {
     return (
         <>
+            
             <h1>HomePage</h1>
+            <p>lsdjalksjdlkasj</p>
+            <Home/>
         </>
     )
 }
